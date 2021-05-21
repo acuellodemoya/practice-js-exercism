d102e3d06d84bfe69ce9d09bed9f521a00b9b6f3
Alejandro Cuello <acuellodemoya@gmail.com>
2021-05-21 15:56:24 -0500

    add resistor-color exercise

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,32 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (color) => {
+  let c = color.toLowerCase();
+
+  if (c == 'black') return (0);
+  if (c == 'brown') return (1);
+  if (c == 'red') return (2);
+  if (c == 'orange') return (3);
+  if (c == 'yellow') return (4);
+  if (c == 'green') return (5);
+  if (c == 'blue') return (6);
+  if (c == 'violet') return (7);
+  if (c == 'grey') return (8);
+  if (c == 'white') return (9);
+};
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
---
+++ resistor-color/resistor-color.spec.js
@@ -0,0 +1,36 @@
+import { colorCode, COLORS } from './resistor-color';
+
+describe('ResistorColor', () => {
+  describe('Color codes', () => {
+    test('Black', () => {
+      expect(colorCode('black')).toEqual(0);
+    });
+
+    test('Black Upper', () => {
+      expect(colorCode('BLACK')).toEqual(0);
+    });
+
+    test('White', () => {
+      expect(colorCode('white')).toEqual(9);
+    });
+
+    test('Orange', () => {
+      expect(colorCode('orange')).toEqual(3);
+    });
+  });
+
+  test('Colors', () => {
+    expect(COLORS).toEqual([
+      'black',
+      'brown',
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'violet',
+      'grey',
+      'white',
+    ]);
+  });
+});
